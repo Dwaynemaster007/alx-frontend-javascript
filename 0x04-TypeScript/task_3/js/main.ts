2890a5d1de5c60b495f8b98539348c0e0e34adce
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-13 13:09:33 +0200

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -11,17 +11,10 @@ const row: RowElement = {
 
 // Insert a row and store the ID
 const newRowID: RowID = CRUD.insertRow(row);
-console.log(`Insert row ${JSON.stringify(row)}`);
 
 // Update the row object
-const updatedRow: RowElement = {
-  ...row,
-  age: 23
-};
+const updatedRow: RowElement = { ...row, age: 23 };
 
 // Call updateRow and deleteRow
 CRUD.updateRow(newRowID, updatedRow);
-console.log(`Update row ${newRowID} ${JSON.stringify(updatedRow)}`);
-
 CRUD.deleteRow(newRowID);
-console.log(`Delete row id ${newRowID}`);
