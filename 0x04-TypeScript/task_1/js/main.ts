dcc47977206caeac9b2350f60ac10a500d92631e
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-13 12:16:42 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,58 +1,18 @@
-// Teacher interface from the previous task
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [propName: string]: any;
+// Interface for the constructor
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-// New Directors interface extending Teacher
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-// Example usage of the Directors interface
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-// Log the director object to the console
-console.log(director1);
-
-// Define the interface for the printTeacher function
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-// Implement the function that matches the interface
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-// Example usage
-console.log(printTeacher("John", "Doe"));
-
-// Interface for the StudentClass constructor
-interface StudentConstructor {
-  new (firstName: string, lastName: string): StudentInterface;
-}
-
-// Interface for the StudentClass methods
-interface StudentInterface {
+// Interface for the class methods
+interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
 }
 
-// The StudentClass implementation
-class StudentClass implements StudentInterface {
-  private firstName: string;
-  private lastName: string;
+// The class implementation
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
 
   constructor(firstName: string, lastName: string) {
     this.firstName = firstName;
@@ -67,8 +27,3 @@ class StudentClass implements StudentInterface {
     return this.firstName;
   }
 }
-
-// Example usage
-const student = new StudentClass('John', 'Doe');
-console.log(student.displayName());
-console.log(student.workOnHomework());
