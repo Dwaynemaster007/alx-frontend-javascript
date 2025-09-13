a904b52953494aa796df807d19e18845896d77c9
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-13 13:09:58 +0200

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,9 +1,5 @@
-// Import types from interface.ts
 import { RowID, RowElement } from './interface';
 
-// Declare types for the functions in crud.js
-declare module './crud' {
-  export function insertRow(row: RowElement): RowID;
-  export function deleteRow(rowId: RowID): void;
-  export function updateRow(rowId: RowID, row: RowElement): RowID;
-}
+declare function insertRow(row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
