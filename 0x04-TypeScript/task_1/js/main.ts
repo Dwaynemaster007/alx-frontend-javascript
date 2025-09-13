b5033bc6cb311e9f9be2759a499cb8a043b27df6
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-13 11:14:58 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,16 +5,16 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [propName: string]: any; // This is the ONLY place this line should be
+  [propName: string]: any;
 }
 
-// New Directors interface extending Teacher
-interface Directors extends Teacher {
+// Corrected Director interface extending Teacher
+interface Director extends Teacher {
   numberOfReports: number;
 }
 
-// Example usage of the Directors interface
-const director1: Directors = {
+// Example usage of the Director interface
+const director1: Director = {
   firstName: 'John',
   lastName: 'Doe',
   location: 'London',
@@ -24,52 +24,3 @@ const director1: Directors = {
 
 // Log the director object to the console
 console.log(director1);
-
-// Define the interface for the printTeacher function
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-// Implement the function that matches the interface
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  const firstLetter = firstName.charAt(0);
-  return `${firstLetter}. ${lastName}`;
-};
-
-// Example usage
-console.log(printTeacher("John", "Doe"));
-
-// Interface for the StudentClass constructor
-interface StudentClassConstructor {
-  new (firstName: string, lastName: string): StudentClass;
-}
-
-// Interface for the StudentClass
-interface StudentClass {
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-// The StudentClass implementation
-class Student implements StudentClass {
-  firstName: string;
-  lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return "Currently working";
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
-}
-
-// Example usage
-const student = new Student('John', 'Doe');
-console.log(student.displayName());
-console.log(student.workOnHomework());
